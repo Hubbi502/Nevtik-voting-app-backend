489353c58e479aedd1f7b6e1d40e31d545fad6ac
Hubbi502 <hubbielfairuz8@gmail.com>
2025-04-19 20:21:53 +0700

    add delete user to route

--- src/routes/auth.router.js
+++ src/routes/auth.router.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { readCSV, uploadCSV } from "../controllers/file.controller.js";
-import { getCurrentUser, getSpecificUser, getUsers, login, logoutUser, addUser } from "../controllers/user.controller.js";
+import { getCurrentUser, getSpecificUser, getUsers, login, logoutUser, addUser, deleteUser } from "../controllers/user.controller.js";
 import { isAuthorized, isDataValid, isLoginValid, isAddUserValid, isAdmin } from "../middleware/auth.middleware.js";
 import { upload } from "../utils/multer.js";
 
@@ -11,7 +11,8 @@ router.get("/user",isAuthorized, getCurrentUser)
 router.post("/login",isLoginValid, login);
 router.post("/addUser",isAddUserValid,isAuthorized ,isAdmin, addUser);
 router.post("/upload/csv", isAuthorized, isAdmin, upload.single('csv'), uploadCSV);
-router.post("/addUser/csv/:filename", isAuthorized,isDataValid,isAdmin, readCSV)
+router.post("/addUser/csv/:filename", isAuthorized,isDataValid,isAdmin, readCSV);
+router.delete("/deleteUser", isAuthorized, isAdmin, deleteUser);
 router.post("/logout", logoutUser)
 router.get("/hello", (req, res) => {
   res.status(200).json({ message: "Hello World" });
